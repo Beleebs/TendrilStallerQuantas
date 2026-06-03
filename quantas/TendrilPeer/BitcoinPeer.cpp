a1332e9c8b501e7535b3292ecbfe590674614170
Ben <150054499+Beleebs@users.noreply.github.com>
2026-06-03 05:46:01 -0400

    change: added incomplete implementations for primary node functionality

--- quantas/TendrilPeer/BitcoinPeer.cpp
+++ quantas/TendrilPeer/BitcoinPeer.cpp
@@ -5,12 +5,176 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 #include "BitcoinPeer.hpp"
-#include "../Common/PowPeer.hpp"
-#include "../Common/Pow.hpp"
+#include "../Common/Peer.hpp"
 
 namespace quantas {
+    static bool registerBitcoinPeer = []() {
+        return PeerRegistry::registerPeerType("BitcoinPeer", [](interfaceId pubId) { 
+            return new BitcoinPeer(new NetworkInterfaceAbstract(pubId)); 
+        });
+    }();
+
+    BitcoinPeer::BitcoinPeer(NetworkInterface* interfacePtr) : Peer(interfacePtr) {
+        // sets random chance for specific node
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    } 
+    BitcoinPeer::BitcoinPeer(const BitcoinPeer& rhs) : Peer(rhs) {}
+
+    // sets the parameters for each node
+    void BitcoinPeer::initParameters(const std::vector<Peer*>& peers, json parameters) {
+        const std::vector<BitcoinPeer*>& bp = reinterpret_cast<const std::vector<BitcoinPeer*>&>(peers); 
+
+        // init the genesis block
+        Bk genesis;
+        genesis.id = 1;
+        genesis.height = 0;
+        genesis.prevID = NULL;
+        genesis.roundMined = 1;
+        genesis.numTx = 1;
+        genesis.txs = { 
+            {1, 1} 
+        };
+        genesis.cbTx = *genesis.txs.begin();
+
+        // setup each node
+        for (auto p : bp) {
+            // change the probability values
+            p->mineProbability_ = parameters.value("mineProbability", mineProbability_);
+            p->txProbability_ = parameters.value("txProbability", txProbability_);
+            
+            // set the top block ID to the genesis node
+            p->topBlockID_ = genesis.id;
+
+            // add genesis to the known blocks and add the coinbase transaction to known txs
+            p->knownBlocks_[genesis.id] = genesis;
+            p->knownTxs_.insert(std::make_pair(genesis.cbTx.id, genesis.cbTx));
+        }
+
+    }
+
+    void BitcoinPeer::performComputation() {
+        /* 
+        steps of computation: 
+        1. Check for incoming transactions/blocks
+            - if transaction found, add to current blockchain top block
+            - add to mempool?
+            - if block found, broadcast it, and add it to blockchain
+            - if fork, uhhhh do something
+        2. Try mining current head block
+            - if mined, broadcast to all connections
+        3. Try making transaction
+            - if successful, broadcast transaction
+        4. Remove confirmed transactions from mempool
+            - not entirely sure how this will go
+            - still pretty unfamiliar with why the mempool exists
+        */
+
+        // 1. Check for incoming transactions/blocks, adding them to knownBlocks/knownTxs/mempool
+        checkInStream();
+
+        // goes through transactions, adds them to the current top block
+        if (!mempool_.empty()) {
+            auto bkIt = knownBlocks_.find(topBlockID_);
+            for (auto& t : mempool_) {
+                // check to see if the transaction is not already confirmed
+                auto txIt = knownTxs_.find(t.id);
+                if (txIt == knownTxs_.end() && topBlockID_ != -1 && bkIt != knownBlocks_.end()) {
+                    // insert transaction into block
+                    bkIt->second.txs.insert(t);
+                    // increment number of transactions in block
+                    ++bkIt->second.numTx;
+                }
+            }
+        }
+
+        // 2. Mine head block
+
+        // 3. Try making transaction
+        // This will be done by the provided txProbability_
+
+        // 4. Remove confirmed transactions from mempool?
+        
+    }
+
+    void BitcoinPeer::endOfRound(std::vector<Peer*>& peers) {
+        // do end of simulation things
+    }
+
+    void BitcoinPeer::checkInStream() {
+        while(!inStreamEmpty()) {
+            Packet packet = popInStream();
+            json msg = packet.getMessage();
+
+            if (msg.contains("type") && msg["type"] == "tx") {
+                // incoming transactions need to be processed
+                // this includes inserting into knownTxs_ and mempool_
+                Tx incomingTx;
+                incomingTx.id = msg["contents"]["txID"];
+                incomingTx.roundSent = msg["contents"]["roundSent"];
+                incomingTx.sender = msg["contents"]["senderID"];
+                incomingTx.receiver = msg["contents"]["receiverID"];
+
+                // check to see if the incoming transaction already exists
+                auto it = knownTxs_.find(incomingTx.id);
+                if (it != knownTxs_.end()) {
+                    // insert into knownTxs and mempool
+                    knownTxs_.insert(std::make_pair(incomingTx.id, incomingTx));
+                    mempool_.insert(incomingTx);
+
+                    // broadcast to all neighbors
+                    broadcast(buildTxMessage(incomingTx));
+                }
+            }
+            else if (msg.contains("type") && msg["type"] == "block") {
+
+            }
+            else if (msg.contains("type") && msg["type"] == "reqtx") {
+                // do later
+            }
+            else if (msg.contains("type") && msg["type"] == "cmpblock") {
+                // do later
+            }
+            else if (msg.contains("type") && msg["type"] == "reqblock") {
+                // do later
+            }
+            
+        }
+    }
+
+    void BitcoinPeer::logMinedBlock(const Bk& block) {
+        json payload = buildBlockMessage(block);
+        LogWriter::pushValue(std::to_string(publicId()), payload);
+    }
+
+    json BitcoinPeer::buildTxMessage(const Tx& transaction) {
+        json result;
+        result["type"] = "tx";
+        result["contents"]["txID"] = transaction.id;
+        result["contents"]["roundSent"] = transaction.roundSent;
+        result["contents"]["senderID"] = transaction.sender;
+        result["contents"]["receiverID"] = transaction.receiver;
+        return result;
+    }
+
+    json BitcoinPeer::buildBlockMessage(const Bk& block) {
+        json result;
+        result["type"] = "block";
+        result["contents"]["blockID"] = block.id;
+        result["contents"]["prevID"] = block.prevID;
+        result["contents"]["height"] = block.height;
+        result["contents"]["roundMined"] = block.roundMined;
+        result["contents"]["numberOfTransactions"] = block.numTx;
+        // log set of transaction ids (int)
+        result["contents"]["transactionIDs"] = json::array();
+        for (auto& t : block.txs) {
+            result["contents"]["transactionIDs"].push_back(t.id);
+        }
+        return result;
+    }
 
     
 }
--- quantas/TendrilPeer/BitcoinPeer.hpp
+++ quantas/TendrilPeer/BitcoinPeer.hpp
@@ -9,11 +9,11 @@
 #include <string>
 #include <set>
 #include <unordered_set>
+#include <deque>
 #include <iostream>
 
 #include "../Common/Peer.hpp"
-#include "../Common/PowPeer.hpp"
-#include "../Common/Pow.hpp"
+#include "../Common/Packet.hpp"
 
 namespace quantas {
     class BitcoinPeer : public Peer {
@@ -29,58 +29,97 @@ namespace quantas {
         void endOfRound(std::vector<Peer*>& peers) override;
     
     private:
-        // transaction interface
+        // Transaction interface
         struct Tx {
+            // comparison overloading
+            // less than
+            bool operator<(const Tx& rhs) {
+                return roundSent < rhs.roundSent;
+            }
+            // equals
+            bool operator==(const Tx& rhs) {
+                return id == rhs.id && roundSent == rhs.roundSent;
+            }
+
+            // Transaction ID
             int id = -1;
+            // Round it was sent on
             int roundSent = -1;
+
+            // Currently, this is not representative of ACTUAL bitcoin.
+            // Simply because Tx's have input/output fields, not necessarily sender/receiver.
+            // for simulation sake, just deal w/ it for now
             interfaceId sender = NO_PEER_ID;
             interfaceId receiver = NO_PEER_ID;
         };
 
         // block structure
         struct Bk {
+            // based on time/round mined
+            bool operator<(const Bk& rhs) {
+                return roundMined < rhs.roundMined;
+            }
+
+            bool operator==(const Bk& rhs) {
+                return id == rhs.id && txs == rhs.txs;
+            }
+
             // header information
-            int id = -1;
-            Bk* prev;
+            int id = -1; 
+            // used for determining fork resolution
+            int height = -1; 
+            // previous id
+            int prevID;
+            // round block was mined
             int roundMined = -1;
 
             // tx information
+            // number of transactions (basically txs.size)
             int numTx = 0;
+            // transactions
             std::set<Tx> txs;
+            // coinbase transaction (the first tx in the block)
+            // not sure if this is specifically the best way to do this? ¯\_(ツ)_/¯
+            Tx cbTx;
         };
 
+        // INPUT CHECK FUNCTIONS
         // checks the input stream for broadcasted messages/blocks/transactions/anything i guess
         void checkInStream();
 
+        // MESSAGE LOGGING FUNCTIONS
         // could include something that logs the blocks into the output json file
         void logMinedBlock(const Bk&);
-        // logs a transaction into a block
-        void logTxIntoBlock(const Tx&, const Bk&);
-        
-        // builds json message for logMinedBlock
+        // builds json message for blocks
         json buildBlockMessage(const Bk&);
-        // builds json message for logTxIntoBlock
+        // builds json message for transactions
         json buildTxMessage(const Tx&);
 
+        // BLOCKCHAIN SPECIFIC FUNCTIONS
         // Known Blocks
-        std::unordered_set<Bk> knownBlocks_;
+        std::unordered_map<int, Bk> knownBlocks_;
         // Known Transactions
-        std::unordered_set<Tx> knownTxs_;
+        std::unordered_map<int, Tx> knownTxs_;
+        // Mempool for unconfirmed transactions
+        std::set<Tx> mempool_;
+        // contains the id of the current block being mined
+        int topBlockID_ = -1;
 
-        // we want some sort of way to mine a block
-        // i think the input should have some sort of modifyable probability value in the input
+        // probability for a block to be mined by the node
         double mineProbability_ = 0.05;
         // number of blocks mined by this node
         int blocksMined_ = 0;
 
-        // For this implementation, BIP 152 (or Compact Block Relay) uses a list of neighbors that can
-        // send over compact blocks before fully confirming the block's validity. [TODO]
+        // probability for transaction to be made
+        double txProbability_ = 0.2;
 
+        // TODO: implement compact block relay
+        // For this implementation, BIP 152 (or Compact Block Relay) uses a list of neighbors that can
+        // send over compact blocks before fully confirming the block's validity.
         // This has a fixed size of 3 peers, as the attackers in TendrilStaller require monopoly of a HBN list.
         // std::vector<Peer*> highBandwidthNeighbors_;
-        
-        // determines whether or not a node transmits at a high bandwidth or low bandwidth
-        bool isHighBandwidth_ = false;
+        // Determines whether or not a node transmits at a high bandwidth or low bandwidth
+        //bool isHighBandwidth_ = false;
     };
 }
 
